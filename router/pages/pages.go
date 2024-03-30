5184e3f43ae92463e5ea9f323fcb7ed942b8a627
Az-Heda <tecno19dragon@gmail.com>
2024-03-30 21:59:36 +0100

    Added an extenral menù for the navbar

--- router/pages/pages.go
+++ router/pages/pages.go
@@ -2,6 +2,14 @@ package pages
 
 import "github.com/gin-gonic/gin"
 
+var navbar [][]string = [][]string{
+	{"Home", "/"},
+	{"Anime", "/anime"},
+	{"Characters", "/character"},
+	{"Dashboard", "/dashboard"},
+}
+
 func Init(r *gin.Engine) {
 	r.GET("/", serveIndex)
+	r.GET("/anime", serveAnime)
 }
