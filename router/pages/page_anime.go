ec96bbea5ae2f3b5eb1463947ce52b864a8f3d9f
Az-Heda <tecno19dragon@gmail.com>
2024-04-04 22:53:29 +0200

    Added more parameters to the template file

--- router/pages/page_anime.go
+++ router/pages/page_anime.go
@@ -3,14 +3,30 @@ package pages
 import (
 	"AniGo/db"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func serveAnime(c *gin.Context) {
 	var animeID string = c.Param("id")
+	var images []string
 
 	anime, err := db.SelectAnimeFromId(animeID)
+	if err != nil {
+		panic(err)
+	}
+
+	if anime.Image.Valid {
+		images = strings.Split(anime.Image.String, "://:")
+	}
+
+	genres, err := db.SelectGenreFromAnimeId(animeID)
+	if err != nil {
+		panic(err)
+	}
+
+	episodes, err := db.SelectEpisodeFromAnimeId(animeID)
 	if err != nil {
 		panic(err)
 	}
@@ -18,7 +34,10 @@ func serveAnime(c *gin.Context) {
 	c.HTML(http.StatusOK, "anime.html", gin.H{
 		"title":          "Anime",
 		"menu":           navbar,
-		"activeMenuItem": "Anime list",
+		"activeMenuItem": "",
 		"anime":          anime,
+		"images":         images,
+		"genres":         genres,
+		"episodes":       episodes,
 	})
 }
