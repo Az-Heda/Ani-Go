5789f9164500ff6416196897d5db4c0d5a03d033
Az-Heda <tecno19dragon@gmail.com>
2024-07-13 17:38:09 +0200

    Updated routes

--- router/pages/pages.go
+++ router/pages/pages.go
@@ -9,6 +9,8 @@ import (
 var navbar [][]string = [][]string{
 	{"Home", "/"},
 	{"Anime list", "/list/anime"},
+	{"Genre list", "/list/genres"},
+	{"Theme list", "/list/themes"},
 	// {"Dashboard", "/dashboard"},
 }
 
@@ -19,9 +21,13 @@ func checkPanic(err error) {
 }
 
 func Init(r *gin.Engine) {
-	r.GET("/", serveIndex)
-	r.GET("/list/anime", serveAnimeList)
+	r.GET(navbar[0][1], serveIndex)
+	r.GET(navbar[1][1], serveAnimeList)
+	r.GET(navbar[2][1], serveGenreList)
+	r.GET(navbar[3][1], serveThemeList)
 
 	r.GET("/anime/:id", serveAnime)
 	r.GET("/character/:id", serveCharacter)
+	r.GET("/genre/:id", serveGenre)
+	r.GET("/theme/:id", serveTheme)
 }
